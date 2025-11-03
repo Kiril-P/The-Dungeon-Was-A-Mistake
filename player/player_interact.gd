481af06ef6110581a3a38a0a489cded57f13d953
Kiril Petrovski <kirilpetrovski2005@gmail.com>
2025-11-03 18:35:10 +0100

    Finished campfire

--- player/player_interact.gd
+++ player/player_interact.gd
@@ -10,7 +10,7 @@ func _physics_process(delta: float) -> void:
 			if !crosshair.visible:
 				crosshair.visible = true
 			if Input.is_action_just_pressed("interact"):
-				hit.get_parent().toggle_light()
+				hit.get_parent().get_parent().toggle_light()
 		
 		elif hit.name == "door":
 			if !crosshair.visible:
